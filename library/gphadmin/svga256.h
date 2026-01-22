fcc07037cdf1f5c1feff84bf05f67228de8ea7a3
samxdev7 <ruedasamuel02@gmail.com>
2026-01-22 13:45:23 -0600

    Moving Function to SVGA256.h

--- library/gphadmin/svga256.h
+++ library/gphadmin/svga256.h
@@ -13,5 +13,13 @@ extern int far _Cdecl Svga256_fdriver[];
 
 /* These are the currently supported modes */
 #define	SVGA320x200x256		0	/* 320x200x256 Standard VGA */
+
+/* Detect SVGA 256 Mode */
+int huge detectar_svga_256(void);
+int huge detectar_svga_256(void)
+{
+    return SVGA320x200x256; /* Case SVGA 320x200x256 */
+}
+
 #endif /* __SVGA256_H__ */
 

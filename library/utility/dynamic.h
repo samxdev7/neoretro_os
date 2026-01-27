68cb2416ccdbd5444ed12068d1915c3f6c52fdc3
samxdev7 <ruedasamuel02@gmail.com>
2026-01-27 16:22:55 -0600

    Translate dynamic.h functions in English Language

--- library/utility/dynamic.h
+++ library/utility/dynamic.h
@@ -1,102 +1,51 @@
+/* DYNAMIC.H - Another useful dynamic memory tool */
 #ifndef DYNAMIC_H
 #define DYNAMIC_H
 
-/*
-	DYNAMIC_H
-	Libreria que administra la memoria dinamica de los programas. 
-	
-    OJO, ES MUY IMPORTANTE TRABAJAR SI O SI CON ESTA LIBRERIA SI QUIERES
-    USAR MEMORIA DINAMICA, DE LO CONTRARIO TE VERAS PERJUDICADO CON LA MEMORIA
-
-	- Samuel Rueda
-*/
-
-/*
-    =======================================
-    Importacion de Librerias Estandar
-    =========================================
-*/
 #include <conio.h>
 #include <graphics.h>
 #include <stdlib.h>
 
-/*
-    =======================================
-    Declaracion de Prototipos
-    =========================================
-*/
-void liberar_elemento(void *ptr);
-void liberar_arreglo(void **ptr, short n_elementos);
-void reservar_opciones_desplegables(char ***opciones_desplegables, short n_opciones);
+void free_element(void *ptr);
+void free_dynamic_array(void **ptr, short n_elements);
+void alloc_deploy_options(char ***deploy_opt, short n_option);
 
-/*
-    =======================================
-    Inicializacion de Funciones
-    =========================================
-*/
-/* 
-	liberar_elemento()
-	- Libera la memoria de un espacio de memoria.
-
-    Parametros:
-    void *ptr: Puntero a espacio de memoria a liberar.
-*/
-void liberar_elemento(void *ptr)
-{
-    /* Validar si no es puntero nulo, luego librerar si se cumplo el caso */
-    if (ptr != NULL) free(ptr);
+void free_element(void *ptr) {
+    if (ptr != NULL) 
+        free(ptr);
 }
 
-/* 
-	liberar_arreglo()
-	- Libera la memoria de varios espacios de memoria.
-
-    Parametros:
-    void **ptr: Puntero a punteros del arreglo.
-    short n_elementos: Cantidad de elementos del arreglo.
-*/
-void liberar_arreglo(void **ptr, short n_elementos)
+void free_dynamic_array(void **ptr, short n_elements)
 {
-    /* 1. Definir ariable de indice */
     unsigned short i; 
 
-    /* 2. Validar si el puntero del arreglo no es nulo */
-    if (ptr == NULL) return;
+    if (ptr == NULL) 
+        return;
 
-    /* 3. Liberar elemento por elemento y asignar a null */
-    for (i = 0; i < n_elementos; i++) {    
-        if (ptr[i] == NULL) return; /* Validar si el puntero del elemento no es nulo */
+    for (i = 0; i < n_elements; i++) {    
+        if (ptr[i] == NULL) 
+            return;
 
         free(ptr[i]);
         ptr[i] = NULL;
     }
 
-    /* 4. Liberar arreglo y asignar a null */
     free(ptr);
     ptr = NULL;
 }
 
-/* 
-	reservar_opciones_desplegables()
-	- Reserva cadenas utilizadas para appadmin.h y las aplicaciones del so (principalmente el 
-    menu de despliegue).
-
-    Parametros:
-    char ***opciones_desplegables: Puntero a matriz de cadenas.
-    short n_opciones: Cantidad de opciones (elementos) del menu desplegable.
-*/
-void reservar_opciones_desplegables(char ***opciones_desplegables, short n_opciones)
+/*
+ * This function is used for create and show a deploy menu,
+ * creating it with dynamic options that the developer
+ * can insert.
+ */
+void alloc_deploy_options(char ***deploy_opt, short n_option)
 {
-    /* 1. Declarar variable de indice */
     unsigned short i;
 
-    /* 2. Reservar a puntero char (**) (matriz de cadenas) */
-    *opciones_desplegables = (char **) malloc(n_opciones * sizeof(char *));
+    *deploy_opt = (char **) malloc(n_option * sizeof(char *));
 
-    /* 3. Reservar cada cadena de la matriz char (*)*/
-    for (i = 0; i < n_opciones; i++) {
-        /* Se reservan 20 caracteres para cada cadena */
-        (*opciones_desplegables)[i] = (char *) malloc(20 * sizeof(char));
-    }
+    for (i = 0; i < n_option; i++)
+        (*deploy_opt)[i] = (char *) malloc(20 * sizeof(char));
 }
 #endif
